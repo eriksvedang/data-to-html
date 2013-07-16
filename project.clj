62194d35552abfb66ef47f53b56422ab41b94e52
Erik <erik.svedang@gmail.com>
2013-07-16 20:11:45 +0200

    Fixed project description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject data-to-html "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject data-to-html "0.1.0"
+  :description "Convert data to html"
+  :url "https://github.com/eriksvedang/data-to-html"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
